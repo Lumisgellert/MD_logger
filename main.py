c502bf3af5acaa2721ed18fa23d01a09db23a3c7
Lumis <lumisgellert@icloud.com>
2025-05-03 14:52:50 +0200

    1.0

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     gps = GPS.GPS()
     while True:
         gps.get_data()
-        print(par.timestamp, par.lon, par.lat, par.satellites)
+        print(par.timestamp, par.lat, par.lon, par.satellites)
