4a4a735d5123dda726af987dc4e6f99850c5d182
Lumis <lumisgellert@icloud.com>
2025-05-03 15:04:19 +0200

    1.0

--- main.py
+++ main.py
@@ -8,6 +8,3 @@ if __name__ == "__main__":
     while True:
         gps.get_data()
         print(par.lat, par.lon, par.satellites, par.altitude, par.speed_kmh, par.speed_knts)
-        if time.time() - now > 1.0:
-            print("hallo")
-            now = time.time()
