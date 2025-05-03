ff74abb59fd8c1ebc68c0d0adc04f6b29e9b8279
Lumis <lumisgellert@icloud.com>
2025-05-03 15:01:20 +0200

    1.0

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import GPS
 import Parameter as par
+import time
 
 if __name__ == "__main__":
     gps = GPS.GPS()
+    now = time.time()
     while True:
         gps.get_data()
         print(par.lat, par.lon, par.satellites, par.altitude, par.speed_kmh, par.speed_knts)
+        if time.time() - now > 1000:
+            print("hallo")
