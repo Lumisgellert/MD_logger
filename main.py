efbee9d8596f96264be21753f9c290f8ff7ede54
Lumis <lumisgellert@icloud.com>
2025-05-03 16:47:29 +0200

    1.0

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     gps = GPS.GPS()
     now = time.time()
     while True:
-            gps.get_data()
-            print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
-
-
+        gps.get_data()
+        print(
+            f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
