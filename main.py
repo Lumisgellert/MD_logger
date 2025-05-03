6609fc2fb7da607fa960b8c3a5da07c0e831d134
Lumis <lumisgellert@icloud.com>
2025-05-03 16:32:05 +0200

    1.0

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     gps = GPS.GPS()
     now = time.time()
     while True:
-        if GPIO.input(17) == GPIO.LOW:
+        while GPIO.input(17) == GPIO.LOW:
             z += 1
             gps.get_data()
             print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
