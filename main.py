09e5b7557f3c2a9fd0b9981b125b0a6b0b5287f2
Lumis <lumisgellert@icloud.com>
2025-05-03 16:06:37 +0200

    1.0

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import GPS
 import Parameter as par
 import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 if __name__ == "__main__":
     gps = GPS.GPS()
     now = time.time()
     while True:
-        gps.get_data()
-        print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
+        if GPIO.input(17) == GPIO.LOW:
+            gps.get_data()
+            print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
