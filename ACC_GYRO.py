78c067344ddd140d5a949390ce8b3ad767094c6b
Lumis <lumisgellert@icloud.com>
2025-05-05 23:19:14 +0200

    1.0

--- ACC_GYRO.py
+++ ACC_GYRO.py
@@ -7,8 +7,11 @@ class MPU6050Sensor:
     def __init__(self, address=0x68):
         self.sensor = mpu6050(address)
 
+        self.sensor.set_accel_range(mpu6050.ACCEL_SCALE_MODIFIER_4G)
+        self.sensor.set_gyro_range(mpu6050.GYRO_RANGE_250DEG)
+
     def read(self):
-        data = self.sensor.get_accel_data()
+        data = self.sensor.get_all_data()
         print(data)
 
 
