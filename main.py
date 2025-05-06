e278529ae8e1ba3d88ce2ea5f55430e4d511c045
Lumis <lumisgellert@icloud.com>
2025-05-06 23:07:57 +0200

    1.1

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ if __name__ == "__main__":
     try:
         gps = GPS.GPS()
         logger = CSVLogger.CSVLogger()
-        schalter = SWITCH.SwitchChecker()
+        schalter = SWITCH.SwitchChecker([16])
         while True:
-            if
-            par.timestamp = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-            gps.get_data()
-            collect_cord(par.lat, par.lon)
-            logger.save([par.timestamp,par.lat, par.lon, par.altitude, par.temp, par.speed_kmh, par.speed_knts, par.course, "Acc_x[g]", "Acc_y[g]", "Acc_z[g]", "Gyro_x[deg/s]", "Gyro_y[deg/s]", "Gyro_z[deg/s]", par.satellites])
-            print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
+            if schalter.pruefe_einzelnen(16) is True:
+                par.timestamp = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+                gps.get_data()
+                collect_cord(par.lat, par.lon)
+                logger.save([par.timestamp,par.lat, par.lon, par.altitude, par.temp, par.speed_kmh, par.speed_knts, par.course, "Acc_x[g]", "Acc_y[g]", "Acc_z[g]", "Gyro_x[deg/s]", "Gyro_y[deg/s]", "Gyro_z[deg/s]", par.satellites])
+                print(f"lat: {par.lat}, lon: {par.lon}, sat: {par.satellites}, alt: {par.altitude}, spd_kmh: {par.speed_kmh}, spd_knts: {par.speed_knts}, Kurs: {par.course}")
     except KeyboardInterrupt:
         show_map()
         plot(logger.get_filepath(), ["v[km/h]", "alt_GND[m]", "sat[n]"])
