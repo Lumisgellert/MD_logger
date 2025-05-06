9849f0fb96598c1051c90c1ca84008f6b51d175a
Lumis <lumisgellert@icloud.com>
2025-05-06 22:28:04 +0200

    1.1

--- ACC_GYRO.py
+++ ACC_GYRO.py
@@ -12,7 +12,18 @@ class MPU6050Sensor:
 
     def read(self):
         par.acc_x = self.sensor.get_accel_data(g=True)["x"]
+        par.acc_y = self.sensor.get_accel_data(g=True)["y"]
+        par.acc_z = self.sensor.get_accel_data(g=True)["z"]
+
+        par.gyro_x = self.sensor.get_gyro_data()["x"]
+        par.gyro_y = self.sensor.get_gyro_data()["y"]
+        par.gyro_z = self.sensor.get_gyro_data()["z"]
         print(par.acc_x)
+        print(par.acc_y)
+        print(par.acc_z)
+        print(par.gyro_x)
+        print(par.gyro_y)
+        print(par.gyro_z)
 
 
 if __name__ == "__main__":
