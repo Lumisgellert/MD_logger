e0cc8ef5c55706ce888864ab5ddd794ff2f1d395
Lumis <lumisgellert@icloud.com>
2025-05-10 17:54:02 +0200

    1.1

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ try:
             led_red.on()
             print(par.time_start)
 
-        elif schalter.pruefe_einzelnen(16) == 1:
+        elif par.S16 == 1:
             par.timestamp = datetime.now().strftime("%d.%m.%Y %H:%M:%S.%f")[:-3]
             gps.get_data()
             mpu.read()
