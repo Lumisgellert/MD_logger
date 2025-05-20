4d0a6b5035f901919b10cd7e86166c54d1b9bb83
jungl <jungluca99@googlemail.com>
2025-05-20 13:52:57 +0200

    mux

--- Parameter.py
+++ Parameter.py
@@ -28,17 +28,3 @@ gyro_x = [0.0]
 gyro_y = [0.0]
 gyro_z = [0.0]
 temp = [0.0]
-
-acc_x2 = 0
-acc_y2 = 0
-acc_z2 = 0
-gyro_x2 = 0
-gyro_y2 = 0
-gyro_z2 = 0
-
-acc_x3 = 0
-acc_y3 = 0
-acc_z3 = 0
-gyro_x3 = 0
-gyro_y3 = 0
-gyro_z3 = 0
--- main.py
+++ main.py
@@ -64,10 +64,10 @@ try:
                 par.timestamp,par.lat, par.lon, par.altitude, par.temp[0], par.speed_kmh,
                 par.speed_knts, par.course,
                 par.acc_x[0], par.acc_y[0], par.acc_z[0], par.gyro_x[0], par.gyro_y[0], par.gyro_z[0],
-                #par.acc_x[1], par.acc_y[1], par.acc_z[1], par.gyro_x[1], par.gyro_y[1], par.gyro_z[1],
-                #par.acc_x[2], par.acc_y[2], par.acc_z[2], par.gyro_x[2], par.gyro_y[2], par.gyro_z[2],
-                #par.acc_x[3], par.acc_y[3], par.acc_z[3], par.gyro_x[3], par.gyro_y[3], par.gyro_z[3],
-                #par.acc_x[4], par.acc_y[4], par.acc_z[4], par.gyro_x[4], par.gyro_y[4], par.gyro_z[4],
+                par.acc_x[1], par.acc_y[1], par.acc_z[1], par.gyro_x[1], par.gyro_y[1], par.gyro_z[1],
+                par.acc_x[2], par.acc_y[2], par.acc_z[2], par.gyro_x[2], par.gyro_y[2], par.gyro_z[2],
+                par.acc_x[3], par.acc_y[3], par.acc_z[3], par.gyro_x[3], par.gyro_y[3], par.gyro_z[3],
+                par.acc_x[4], par.acc_y[4], par.acc_z[4], par.gyro_x[4], par.gyro_y[4], par.gyro_z[4],
                 par.satellites
             ])
             print(
@@ -83,8 +83,12 @@ try:
                 threading.Thread(target=led_blue.blink_fast, daemon=True).start()
                 show_map()
                 plot(filepath, [
-                    "v[kmh]", "alt_GND[m]", "sat[n]", "Acc_x[g]", "Acc_y[g]", "Acc_z[g]",
-                    "Gyro_x[deg_s]", "Gyro_y[deg_s]", "Gyro_z[deg_s]"
+                    "v[kmh]", "alt_GND[m]", "sat[n]", "temp[°C]",
+                    "Acc_x0[g]", "Acc_y0[g]", "Acc_z0[g]", "Gyro_x0[deg_s]", "Gyro_y0[deg_s]", "Gyro_z0[deg_s]",
+                    "Acc_x1[g]", "Acc_y1[g]", "Acc_z1[g]", "Gyro_x1[deg_s]", "Gyro_y1[deg_s]", "Gyro_z1[deg_s]",
+                    "Acc_x2[g]", "Acc_y2[g]", "Acc_z2[g]", "Gyro_x2[deg_s]", "Gyro_y2[deg_s]", "Gyro_z2[deg_s]",
+                    "Acc_x3[g]", "Acc_y3[g]", "Acc_z3[g]", "Gyro_x3[deg_s]", "Gyro_y3[deg_s]", "Gyro_z3[deg_s]",
+                    "Acc_x4[g]", "Acc_y4[g]", "Acc_z4[g]", "Gyro_x4[deg_s]", "Gyro_y4[deg_s]", "Gyro_z4[deg_s]",
                 ])
                 save()
             par.check_bit = False
