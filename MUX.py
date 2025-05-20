3e5a65ff26bb8ba7bfcce445f10d9c1d890fe13e
jungl <jungluca99@googlemail.com>
2025-05-20 11:19:41 +0200

    mux

---
+++ MUX.py
@@ -0,0 +1,12 @@
+import smbus2
+
+class TCA9548A:
+    def __init__(self, bus=1, address=0x70):
+        self.bus = smbus2.SMBus(bus)
+        self.address = address
+
+    def select_channel(self, channel):
+        if 0 <= channel <= 7:
+            self.bus.write_byte(self.address, 1 << channel)
+        else:
+            raise ValueError("Kanal muss zwischen 0 und 7 liegen")
