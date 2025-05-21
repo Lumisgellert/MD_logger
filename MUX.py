4fd7b083c120c6d4bc23c040be67bfdfd3ade13b
Lumis <lumisgellert@icloud.com>
2025-05-21 19:49:23 +0200

    1.1

--- MUX.py
+++ MUX.py
@@ -1,13 +1,31 @@
-import smbus2
+import board
+import busio
 
 
 class TCA9548A:
-    def __init__(self, bus=1, address=0x70):
-        self.bus = smbus2.SMBus(bus)
+    def __init__(self, i2c, address=0x70):
+        self.i2c = i2c
         self.address = address
 
     def select_channel(self, channel):
         if 0 <= channel <= 7:
-            self.bus.write_byte(self.address, 1 << channel)
+            data = bytes([1 << channel])
+            while not self.i2c.try_lock():
+                pass
+            try:
+                self.i2c.writeto(self.address, data)
+            finally:
+                self.i2c.unlock()
         else:
             raise ValueError("Kanal muss zwischen 0 und 7 liegen")
+
+
+if __name__ == "__main__":
+    # Initialisiere I2C mit board-Standard
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # Erzeuge Multiplexer-Instanz
+    mux = TCA9548A(i2c, address=0x70)
+
+    # Kanal 0 aktivieren
+    mux.select_channel(0)
