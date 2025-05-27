5aa83f40b06b0fc49004909b450ae387e18aae2a
Lumis <lumisgellert@icloud.com>
2025-05-27 21:39:38 +0200

    1.1

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ try:
             #gps.get_data()
             for i, sensor in enumerate(sensors):
                 sensor.read(index=i)
-                sleep(0.05)
+                sleep(0.025)
 
             collect_cord(par.lat, par.lon)
             logger.save([
