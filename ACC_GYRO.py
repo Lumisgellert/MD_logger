0b44ab49648ceb2e8aa1ce3649331c504c064179
Lumis <lumisgellert@icloud.com>
2025-05-30 13:18:29 +0200

    1.1

--- ACC_GYRO.py
+++ ACC_GYRO.py
@@ -29,24 +29,27 @@ class MPU6050Sensor:
             return False
 
     def read(self, index):
-        self.mux.select_channel(self.channel)
-        acc = self.sensor.get_accel_data(g=True)
-        gyro = self.sensor.get_gyro_data()
+        try:
+            self.mux.select_channel(self.channel)
+            acc = self.sensor.get_accel_data(g=True)
+            gyro = self.sensor.get_gyro_data()
 
-        par.acc_x[index] = acc["x"]
-        par.acc_y[index] = acc["y"]
-        par.acc_z[index] = acc["z"]
+            par.acc_x[index] = acc["x"]
+            par.acc_y[index] = acc["y"]
+            par.acc_z[index] = acc["z"]
 
-        par.gyro_x[index] = gyro["x"]
-        par.gyro_y[index] = gyro["y"]
-        par.gyro_z[index] = gyro["z"]
-        """
-        par.acc_x[index] = acc["x"] + 0.006
-        par.acc_y[index] = acc["y"] + 0.003
-        par.acc_z[index] = acc["z"] + 0.012
-
-        par.gyro_x[index] = gyro["x"] + 1.52
-        par.gyro_y[index] = gyro["y"] - 1.30
-        par.gyro_z[index] = gyro["z"] - 2.19
-        """
-        par.temp[index] = self.sensor.get_temp()
+            par.gyro_x[index] = gyro["x"]
+            par.gyro_y[index] = gyro["y"]
+            par.gyro_z[index] = gyro["z"]
+            """
+            par.acc_x[index] = acc["x"] + 0.006
+            par.acc_y[index] = acc["y"] + 0.003
+            par.acc_z[index] = acc["z"] + 0.012
+    
+            par.gyro_x[index] = gyro["x"] + 1.52
+            par.gyro_y[index] = gyro["y"] - 1.30
+            par.gyro_z[index] = gyro["z"] - 2.19
+            """
+            par.temp[index] = self.sensor.get_temp()
+        except Exception as e:
+            raise RuntimeError(e)
