1a54d520e95cdba94d44f313d776990ff5b38ed2
Lumis <lumisgellert@icloud.com>
2025-05-30 13:42:37 +0200

    1.1

--- ACC_GYRO.py
+++ ACC_GYRO.py
@@ -34,22 +34,23 @@ class MPU6050Sensor:
             acc = self.sensor.get_accel_data(g=True)
             gyro = self.sensor.get_gyro_data()
 
-            par.acc_x[index] = acc["x"]
-            par.acc_y[index] = acc["y"]
-            par.acc_z[index] = acc["z"]
+            if index == 0:
+                par.acc_x[index] = acc["x"]
+                par.acc_y[index] = acc["y"]
+                par.acc_z[index] = acc["z"] * -1
 
-            par.gyro_x[index] = gyro["x"]
-            par.gyro_y[index] = gyro["y"]
-            par.gyro_z[index] = gyro["z"]
-            """
-            par.acc_x[index] = acc["x"] + 0.006
-            par.acc_y[index] = acc["y"] + 0.003
-            par.acc_z[index] = acc["z"] + 0.012
-    
-            par.gyro_x[index] = gyro["x"] + 1.52
-            par.gyro_y[index] = gyro["y"] - 1.30
-            par.gyro_z[index] = gyro["z"] - 2.19
-            """
+                par.gyro_x[index] = gyro["x"]
+                par.gyro_y[index] = gyro["y"]
+                par.gyro_z[index] = gyro["z"]
+            else:
+                par.acc_x[index] = acc["x"]
+                par.acc_y[index] = acc["y"]
+                par.acc_z[index] = acc["z"]
+
+                par.gyro_x[index] = gyro["x"]
+                par.gyro_y[index] = gyro["y"]
+                par.gyro_z[index] = gyro["z"]
+
             par.temp[index] = self.sensor.get_temp()
 
         except OSError as e:
