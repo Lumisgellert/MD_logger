3a7dc74f66d628e7002e4c476ff6e53207ad504a
Lumis <lumisgellert@icloud.com>
2025-06-01 17:05:12 +0200

    1.1

--- reboot.py
+++ reboot.py
@@ -3,5 +3,5 @@ import time
 
 if __name__ == "__main__":
     print("Neustart wird ausgeführt...")
-    os.system('sudo reboot')
+    os.system('python3 /home/Data/Desktop/MD_logger/main.py')
     time.sleep(0.1)
