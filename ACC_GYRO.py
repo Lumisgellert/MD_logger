a3a9148aad0de69519df56bd2d8359716cea832b
Lumis <lumisgellert@icloud.com>
2025-06-08 16:14:03 +0200

    1.1

--- ACC_GYRO.py
+++ ACC_GYRO.py
@@ -1,4 +1,4 @@
-from mpu6050 import mpu6050
+from MPU6050 import mpu6050
 import Parameter as par
 import time
 import numpy as np
--- Live_plot.py
+++ Live_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 import Parameter as par
-from mpu6050 import MPU6050Sensor
+from MPU6050 import MPU6050Sensor
 from I2CMULTIPLEXER import I2CMultiplexer
 
 # Puffer für die letzten N Werte
